024688fc8cc6d8987c1198d2727e10c0c0e7e522
Criptdestroyer <ahmademir1201@gmail.com>
2019-03-18 23:01:50 +0700

    add Parking_lot and Car class

---
+++ src/main/java/com/gojek/Car.java
@@ -0,0 +1,19 @@
+package com.gojek;
+
+/*
+ * author : Ahmad Emir Alfatah
+ */
+
+ public class Car
+ {
+     private String regNumber;
+     private String colour;
+
+     public Car(String regNumber, String colour)
+     {
+         this.regNumber = regNumber;
+         this.colour = colour;
+     }
+
+     
+ }
---
+++ src/main/java/com/gojek/Parking_lot.java
@@ -0,0 +1,10 @@
+package com.gojek;
+
+/*
+ * author : Ahmad Emir Alfatah
+ */
+
+public class Parking_lot
+{
+   private int MAX_SIZE = 0; 
+}
