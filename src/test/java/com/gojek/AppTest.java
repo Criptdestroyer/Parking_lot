ce317c3742bc331141589eb3ce7ec4e207423014
Criptdestroyer <ahmademir1201@gmail.com>
2019-03-24 11:46:49 +0700

    done

---
+++ src/test/java/com/gojek/AppTest.java
@@ -0,0 +1,19 @@
+package com.gojek;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    
+}
